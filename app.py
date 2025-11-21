6c612ba8fa0883659fa8345ca3263b95b6eae3c3
Nilesh <service6313@gmail.com>
2025-11-21 17:30:34 +0530

    test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello from Techcorp Backend!"
+    return "hello from darling !"
 
-if __name__ == "__main__";
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
